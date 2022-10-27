3d7d11693e08c4170e30aff8d55e751b670ff5e1
AlexNica <alexandru.nica2@student.upt.ro>
2022-10-27 13:08:32 +0300

    Admin page added

--- StudentHub/admin.py
+++ StudentHub/admin.py
@@ -2,21 +2,20 @@ from django.contrib import admin
 from .models import HubPageDataModel, ChatMessages
 
 
-# class ChatMessagesInline(admin.TabularInline):
-#     model = ChatMessages
-#
-#
-# class HubPageDataModelAdmin(admin.ModelAdmin):
-#     fieldsets = [
-#         (None, {'fields': ['title', 'subject', 'author']}),
-#         ('Date information', {'fields': ['date', 'date_end']}),
-#         ('Description', {'fields': ['description']})
-#     ]
-#     inlines = [ChatMessagesInline]
-#     list_display = ('title', 'subject', 'date_end')
-#     list_filter = ['date_end']
-#     search_fields = ['title']
-#
-#
-# admin.site.register(HubPageDataModel, HubPageDataModelAdmin)
-# # Register your models here.
+class ChatMessagesInline(admin.TabularInline):
+    model = ChatMessages
+
+
+class HubPageDataModelAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'subject', 'author']}),
+        ('Date information', {'fields': ['date', 'date_end']}),
+        ('Description', {'fields': ['description']})
+    ]
+    list_display = ('title', 'subject', 'date_end')
+    list_filter = ['date_end']
+    search_fields = ['title']
+
+
+admin.site.register(HubPageDataModel, HubPageDataModelAdmin)
+# Register your models here.
--- StudentHub/models.py
+++ StudentHub/models.py
@@ -9,9 +9,15 @@ class HubPageDataModel(models.Model):
     date_end = models.DateTimeField(['%Y-%m-%d %H:%M'], max_length=255, default='empty')
     description = models.TextField(default='empty')
 
+    def __str__(self):
+        return self.title
+
 
 class ChatMessages(models.Model):
     messageText = models.TextField()
     user = models.CharField(max_length=255)
     subject = models.CharField(max_length=255)
     room_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.messageText
