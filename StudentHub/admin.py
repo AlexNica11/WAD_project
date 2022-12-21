8914e1c494fac9be8e6d06ef2293ebb6d14c5918
AlexNica <alexandru.nica2@student.upt.ro>
2022-12-21 20:58:51 +0200

    contacts admin page added

--- StudentHub/admin.py
+++ StudentHub/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import HubPageDataModel, ChatMessages, Questions
+from .models import HubPageDataModel, ChatMessages, Questions, Contacts
 
 
 class ChatMessagesInline(admin.TabularInline):
@@ -23,6 +23,24 @@ class HubPageDataModelAdmin(admin.ModelAdmin):
 admin.site.register(HubPageDataModel, HubPageDataModelAdmin)
 
 
+class QuestionsInline(admin.TabularInline):
+    model = Questions
+
+
+class ContactsAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['full_name', 'contact_phone']}),
+        ('Developer', {'fields': ['dev_id']})
+    ]
+    inlines = [QuestionsInline]
+    list_display = ('full_name', 'contact_phone')
+    list_filter = ['full_name']
+    search_fields = ['full_name']
+
+
+admin.site.register(Contacts, ContactsAdmin)
+
+
 class QuestionsAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['title', 'date', 'user_id', 'contact_id']}),
@@ -34,5 +52,7 @@ class QuestionsAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Questions, QuestionsAdmin)
+
+
 
 # Register your models here.
